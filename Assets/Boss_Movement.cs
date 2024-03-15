1c11f15b5ce7ac705af90aa8946d4c3bf75afc68
adamwi1000 <adamwi1000@gmai.com>
2024-03-15 21:28:49 +0100

    kok

---
+++ Assets/Boss_Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Movement : MonoBehaviour
+{
+    public GameObject Point_a;
+    public GameObject Point_b;
+    public Rigidbody2D rb;
+    public Transform current_point;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
