b1fb6c1f4eab33487ba680b710f4fc2690dec3c6
MaroneQ <madragon.gamingg@gmail.com>
2024-03-16 02:16:35 +0100

    kupa

---
+++ Assets/Player_movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb2d;
+    private Vector2 moveInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+    }
+
+    void FixedUpdate() 
+    { 
+        rb2d.MovePosition(rb2d.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
